452f8dd673e9823240bfa2dc8267a9a878c88c7a
hardik-kedar <hardik.kedar2004@gmail.com>
2026-03-09 18:43:51 +0530

    fix cors for production

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ import walletRoutes from "./routes/wallet.routes";
 import transactionRoutes from "./routes/transaction.routes";
 import withdrawalRoutes
 from "./routes/withdrawal.routes";
-
+import cors from "cors";
 
 
 const PORT = process.env.PORT || 5000;
@@ -44,12 +44,33 @@ CONNECT DATABASE
 */
 connectDB();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://trycompanio.in",
+      "https://www.trycompanio.in",
+      "https://companio-frontend.vercel.app"
+    ],
+    credentials: true
+  })
+);
 /*
 ================================
 REGISTER ROUTES
 ================================
 */
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://trycompanio.in",
+      "https://www.trycompanio.in",
+      "https://companio-frontend.vercel.app"
+    ],
+    credentials: true
+  })
+);
 app.use("/api/conversations", conversationRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/admin/disputes", adminDisputeRoutes);
@@ -75,7 +96,12 @@ CREATE SOCKET SERVER
 */
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+  "http://localhost:3000",
+  "https://trycompanio.in",
+  "https://www.trycompanio.in",
+  "https://companio-frontend.vercel.app"
+],
     credentials: true,
     methods: ["GET", "POST"]
   }
